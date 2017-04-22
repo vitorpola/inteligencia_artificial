093c5b7d91d5dff8f59139abeaae31caf184bb94
Vitor Pola <vitorpola@gmail.com>
2017-04-21 23:35:31 -0300

    version 2

--- perceptron.rb
+++ perceptron.rb
@@ -2,7 +2,7 @@
 def read_file(file_name)
     a =[]
     File.open(file_name) do |f|
-        f.lines.each do |line|
+        f.each_line.each do |line|
             a << line.split(',').map(&:to_i)
         end
     end
@@ -18,15 +18,81 @@ def fill_array(array, init, final)
     return a
 end
 
+def result_convert(num)
+    if num == 1
+        return [ 1,-1,-1]
+    elsif num == 2
+        return [-1, 1,-1]
+    else
+        return [-1,-1, 1]
+    end
+end
+
+def init_weights
+    weights = []
+    for i in 0..2 
+        weights << []
+        for j in 0..8
+            weights[i] << 0
+        end
+    end
+    weights
+end
+
 #todos dados
 all_data = read_file('cmc.data.txt')
 
-#dados treino com classe 1
-trainning_1 = fill_array(all_data, 0, 414)
-#dados treino com classe 2
-trainning_2 = fill_array(all_data, 415, 641)
-#dados treino com classe 3
-trainning_3 = fill_array(all_data, 642, 999)
+# dados treino
+#      classe 1: 0   - 414
+#      classe 2: 415 - 641
+#      classe 3: 642 - 999
+trainning = fill_array(all_data, 0, 999)
 
-#dados teste com todas classes
+# dados teste com todas classes
 test = fill_array(all_data, 1000, all_data.count-1)
+
+@weights = init_weights     # pesos
+@alpha = 0.4                # somatorio das entradas * pesos
+@right_answers = 0;
+
+def perceptron(array_data, is_test, tmax)
+    for t in 1..tmax
+        if is_test
+            line = t-1
+        else
+            line = Random.rand(0..999)
+        end
+
+        x = []
+        for i in 0..8 
+            x << array_data[line][i]
+        end
+        x << 1
+        se = result_convert(array_data[line][9])
+
+        y_in = [0,0,0]
+        
+        for i in 0..2 
+            for j in 0..8
+                y_in[i] += x[j] * @weights[i][j]
+            end
+        end
+
+        y = []
+        y_in.each { |i| y << (i >= 0 ? 1 : -1) }
+
+        if y != se && !is_test
+            for i in 0..2
+                for j in 0..8
+                     @weights[i][j] += @alpha * ( se[i] - y[i] ) * x[j] 
+                end   
+            end
+        end
+
+        @right_answers += 1 if y == se && is_test
+    end
+    puts "Porcentagem de acerto -> #{ 100* @right_answers /tmax }%" if is_test
+end
+
+perceptron(trainning, false, 100)
+perceptron(test, true, test.count)
